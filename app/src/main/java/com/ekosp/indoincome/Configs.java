8f16f21b7765cab6b6b6504c129280c50b336e67
ekosp <jecksr@gmail.com>
2017-11-17 11:00:33 +0700

    Feat : Init project indoincome android apps
    The first version only display web content "As Is"  using Android WebView.
    The feature is :
     * splashscreeen on open app
     * web view blog indoincome
     * double back press to exit app
     * display custom error when faild to load url

---
+++ app/src/main/java/com/ekosp/indoincome/Configs.java
@@ -0,0 +1,12 @@
+package com.ekosp.indoincome;
+
+/**
+ * Created by Eko Setyo Purnomo on 17-Nov-17.
+ * Find me on https://ekosp.com
+ * Or email me directly to ekosetyopurnomo@gmail.com
+ */
+
+public class Configs {
+    public static String WEBSITE_PATH = "https://indoincome.com";
+
+}
